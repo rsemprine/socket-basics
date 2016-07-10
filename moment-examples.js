38783143d907f393efae62a56485dce74e888295
Rodrigo Fonseca <rsemprine@gmail.com>
2016-07-10 13:12:12 -0300

    Update moment example

--- moment-examples.js
+++ moment-examples.js
@@ -1,8 +1,16 @@
 var moment = require('moment');
 var now = moment();
-
+/*
 console.log(now.format());
-console.log(now.format('X'));
+console.log(now.format('X')); //seconds
+console.log(now.format('x')); //miliseconds
+console.log(now.valueOf()); //miliseconds
+*/
+var timestamp = 1468166727519;
+var timestampMoment = moment.utc(timestamp); //hora UTC/GMT
+
+console.log(timestampMoment.format('h:mma'));
+console.log(timestampMoment.local().format('h:mma')); //hora local
 
 /*
 now.subtract(1, 'year');
