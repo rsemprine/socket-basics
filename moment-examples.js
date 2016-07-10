e835a5864764b210ed9b05c946a8ae5c7d497e74
Rodrigo Fonseca <rsemprine@gmail.com>
2016-07-10 13:03:05 -0300

    Add moment

--- moment-examples.js
+++ moment-examples.js
@@ -2,7 +2,11 @@ var moment = require('moment');
 var now = moment();
 
 console.log(now.format());
+console.log(now.format('X'));
 
+/*
 now.subtract(1, 'year');
+
 console.log(now.format());
 console.log(now.format('MMM Do YYYY, h:mma'));
+*/
